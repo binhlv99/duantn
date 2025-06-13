e95fc0794dafdaf24fab4fa077c4da36fe01b3ba
binhlv99 <binhlv_it@outlook.com>
2025-06-13 18:21:58 +0700

    Add hoa don controller

---
+++ demo/demo/src/main/java/com/poly/demo/controller/HoaDonController.java
@@ -0,0 +1,38 @@
+package com.poly.demo.controller;
+
+import com.poly.demo.repository.HoaDonRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HoaDonController {
+    @Autowired
+    private HoaDonRepo hoaDonRepo;
+
+    @GetMapping("/list-hoa-don")
+    public String getHoaDon(Model model) {
+/*
+        // Tao tai khoan admin ban dau
+        var accountCreate = TaiKhoan.builder()
+                .email("binhlv44842@fpt.edu.vn")
+                .cccd("022099002948")
+                .ma("44842")
+                .hoTen("Le Van Binh")
+                .soDienThoai(829296511)
+                .tenDangNhap("admin")
+                .matKhau("123456")
+                .idVaiTro(1)
+                .ngaySinh(LocalDateTime.now())
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .build();
+
+        taiKhoanRepo.save(accountCreate);*/
+        model.addAttribute("ListHoaDon", hoaDonRepo.findAll());
+        return "TAIKHOAN/hien-thi";
+    }
+
+
+}
